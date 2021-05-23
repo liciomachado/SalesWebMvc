a8e50aa0699916e89bf96f6b9ec0d34f802b25c3
Mauricio <licio.machado@hotmail.com>
2021-05-23 15:06:05 -0300

    primeiro model-controller-view Department

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -18,7 +18,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
+            ViewData["email"] = "marucio@gmail.com";
             return View();
         }
 
